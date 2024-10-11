3c0a31a28f528eda301ff4ef186055f086d6e5d1
Vitor-sbaptista <vitorbaptista9107@gmail.com>
2024-10-10 21:17:04 -0300

    exec-4.py

--- exec-4.py
+++ exec-4.py
@@ -1,4 +1,4 @@
-Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor.
+#Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor.
 
 
 Numero = int(input("Digite um número para saber seu antecessor."))
