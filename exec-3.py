0b0ec0b85951e3327721c8a3466b895e1de2f744
Vitor-sbaptista <vitorbaptista9107@gmail.com>
2024-10-10 21:16:35 -0300

    exec-3.py

--- exec-3.py
+++ exec-3.py
@@ -1,4 +1,4 @@
-3) Os pares de instruções abaixo produzem o mesmo resultado? Imprima os valores abaixo, veja se algum deles (A, B ou C) possuem valores diferentes nas versões 1 e 2, e caso sim, explique num comentário o motivo.
+#3) Os pares de instruções abaixo produzem o mesmo resultado? Imprima os valores abaixo, veja se algum deles (A, B ou C) possuem valores diferentes nas versões 1 e 2, e caso sim, explique num comentário o motivo.
 
 A1 ← (4/2)+(2/4)	e	A2 ← 4/2+2/4
 B1 ← 4/(2+2)/4	e	B2 ← 4/2+2/4
