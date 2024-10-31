ef44c5c107d7e8f88e7590ef17477ae3b4aa0b08
Vitor-sbaptista <vitorbaptista9107@gmail.com>
2024-10-31 20:38:35 -0300

    exec-10.py

---
+++ exec-10.py
@@ -0,0 +1,14 @@
+'''
+Uma revendedora de carros usados paga a seus funcionários vendedores um salário fixo por mês, mais uma comissão também fixa de R$ 700,00 para cada carro vendido e mais 5% do valor das vendas por 
+ele efetuadas. Escrever um algoritmo que leia o número de carros por ele vendidos, o valor total de suas vendas, o saláriofixo e o valor que ele recebe por carro vendido. Calcule e escreva o salário final do vendedor. 
+'''
+salarioFixo = int(input("Qual o salário fixo do vendedor? "))
+comissaoFixo = 700
+quantidadeCarrosVendidos = int(input("Quantos carros foram vendidos? "))
+totalComissaoFixo = comissaoFixo * quantidadeCarrosVendidos
+
+salarioSemComissao = float(salarioFixo + totalComissaoFixo)
+adcional_5 = salarioSemComissao * 0.05
+salarioFinal = salarioSemComissao + adcional_5
+
+print("O salário final do vendedor é: ", salarioFinal)
