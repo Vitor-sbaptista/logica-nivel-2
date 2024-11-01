014c9c7c95750d90ac912becc2da7c8997dc69e9
Vitor-sbaptista <vitorbaptista9107@gmail.com>
2024-11-01 19:18:58 -0300

    exec-19.py

---
+++ exec-19.py
@@ -0,0 +1,11 @@
+'''
+19) Ler dois valores (considere que não serão lidos valores iguais) e 
+escrevê-los em ordem crescente.
+'''
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite um número: "))
+
+if numero_1 > numero_2:
+    print(numero_2, numero_1)
+else:
+    print(numero_1, numero_2)
