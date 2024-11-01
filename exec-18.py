c2a07d7741880f175c29021626fb4f9e62c5d01b
Vitor-sbaptista <vitorbaptista9107@gmail.com>
2024-11-01 18:14:04 -0300

    exec-18.py

---
+++ exec-18.py
@@ -0,0 +1,10 @@
+'''
+18) Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles. 
+'''
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número: "))
+
+if numero_1 > numero_2:
+    print(numero_1)
+else:
+    print(numero_2)
