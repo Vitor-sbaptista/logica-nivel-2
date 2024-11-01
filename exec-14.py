0772735d293251d63f88e4f5562701a49ee3726f
Vitor-sbaptista <vitorbaptista9107@gmail.com>
2024-11-01 00:28:03 -0300

    exec-14.py

---
+++ exec-14.py
@@ -0,0 +1,10 @@
+'''
+14) Ler um valor e escrever se é positivo ou negativo(considere o valor zero
+como positivo).
+'''
+x = int(input("Digite um número: "))
+
+if x>=0:
+    print("O valor é positivo")
+else:
+    print("O valor é negativo")
